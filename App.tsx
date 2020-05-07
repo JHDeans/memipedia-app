ff780eb555188d024310e152f4e8371e4ff3f724
Joel Deans <JHDeans@users.noreply.github.com>
2020-05-07 14:14:30 -0600

    initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import FeedScreen from "./screens/FeedScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FeedScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
